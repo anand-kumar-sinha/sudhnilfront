9a1ee5f660ffbef5088f390a0ea60a3d690bce35
Priya-kumari71 <priyakumarinwd100@gmail.com>
2025-05-03 18:23:51 +0530

    ADD profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Collection from "./pages/Collection.jsx";
-import About from "./pages/About.jsx";
-import Contact from "./pages/Collection.jsx";
-import Product from "./pages/Product.jsx";
-import Cart from "./pages/Cart.jsx";
-import Login from "./pages/Login.jsx";
-import PlaceOrder from "./pages/PlaceOrder.jsx";
-import Orders from "./pages/Orders.jsx";
-import Verify from "./pages/Verify.jsx";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Collection";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+import Verify from "./pages/Verify";
+import Profile from './pages/Profile';
 
 const App = () => {
   return (
     <Routes>
-      
-      {/* created 9 route for 9 pages */}
-      <Route path="/" element={<Home />} />
-      <Route path="collection" element={<Collection />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-
-      {/* using product Id we will get particular product on page */}
-      <Route path="/product/:productId" element={<Product />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="login" element={<Login />} />
-      <Route path="place-order" element={<PlaceOrder />} />
-      <Route path="/orders" element={<Orders />} />
-      <Route path="/verify" element={<Verify />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/verify" element={<Verify />} />
+        <Route path="/profile" element={<Profile />} />
+      </Route>
     </Routes>
   );
 };
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,18 @@
+// Layout.jsx
 import { ToastContainer } from "react-toastify";
 import Navbar from "./components/Navbar";
 import SearchBar from "./components/SearchBar";
 import BottomNavBar from "./components/BottomNavBar";
 import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <div className="px-4 sm:px-5[vw] md:px-7[vw] lg:px-[9vw] mb-16">
       <ToastContainer />
       <Navbar />
       <SearchBar />
-      {children}
+      <Outlet /> {/* This renders the current route */}
       <Footer />
       <BottomNavBar />
     </div>
