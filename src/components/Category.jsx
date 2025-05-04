0dc26ced6ffe71958a6c47fc4e43ecb28b0b1e5a
anand kumar <anandkumar7368970125@gmail.com>
2025-05-04 14:10:55 +0530

    xvc

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -13,25 +13,29 @@ const Category = () => {
           </h2>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
             {/* Category Card */}
+            {
+              console.log(categories)
+            }
 
-            {/* {categories &&
+            {categories &&
               categories
                 ?.slice(0, 4)
                 .map((category) => (
                   <CategoryCard key={category._id} category={category} />
-                ))} */}
+                ))}
 
-
-<div className="bg-white shadow-md rounded-2xl overflow-hidden hover:shadow-lg transition">
-      <img
-        src='sdlfjld'
-        alt="Electronics"
-        className="w-full h-40 object-cover"
-      />
-      <div className="p-4">
-        <h3 className="text-lg font-semibold text-gray-800">sdfdfsdfdf</h3>
-      </div>
-    </div>
+            <div className="bg-white shadow-md rounded-2xl overflow-hidden hover:shadow-lg transition">
+              <img
+                src="sdlfjld"
+                alt="Electronics"
+                className="w-full h-40 object-cover"
+              />
+              <div className="p-4">
+                <h3 className="text-lg font-semibold text-gray-800">
+                  sdfdfsdfdf
+                </h3>
+              </div>
+            </div>
           </div>
         </div>
       </section>
