d7f3be7db5d8314c1a842b59693cf1b783d1e4e7
anand kumar <anandkumar7368970125@gmail.com>
2025-05-15 20:58:02 +0530

    change local url

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -5,8 +5,8 @@ import axios from "axios";
 
 export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
-  // const backandUrl = "http://localhost:4000";
-  const backandUrl = "https://ecomm-backend-tau.vercel.app";
+  const backandUrl = "http://localhost:4000";
+  // const backandUrl = "https://ecomm-backend-tau.vercel.app";
   const currency = "₹";
   const delivery_fee = 40;
 
